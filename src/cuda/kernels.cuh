5b30243e709ca0b92a87643b9994f37d3e878adc
Hengda Shi <hengda.shi@engineering.ucla.edu>
2019-12-09 20:47:47 +0000

    merged zongze branch

--- src/cuda/kernels.cuh
+++ src/cuda/kernels.cuh
@@ -3,6 +3,8 @@
 
 #include "variable.h"
 #include "sparse.h"
+#include <thrust/extrema.h>
+#include <thrust/device_ptr.h>
 
 __global__
 void GraphSum_forward_kernel(float *d_in, float *d_out, int *d_indptr, int *d_indices, int dim, int numNodes);
@@ -12,6 +14,9 @@ __global__
 void GraphSum_backward_kernel(float *d_in, float *d_out, int *d_indptr, int *d_indices, int dim, int numNodes);
 void GraphSum_backward(Variable *in, Variable *out, SparseIndex *graph, int dim);
 
+__global__
+void CrossEntropy_forward_kernel(float* logits_data, float* logits_grad, bool training, int num_classes, int* truth, int* count, float* thread_loss, int size);
+void CrossEntropy_forward(Variable *logits, int *truth, float &total_loss, int &count, int num_classes, bool training);
 
 __global__
 void ReLU_forward_kernel(float *d_in, bool *d_mask, const long unsigned int datasize, bool training);
